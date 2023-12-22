663fb4c9461d06f45cb238e3fd1080ed7f84bd1c
AlexBissing <avbissing@yandex.ru>
2023-12-22 20:10:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def main():
+    try:
+        while(True):
+            put = input("Введите выражение: ")
+            sl = put.split()
+            a, op, b = int(sl[0]), sl[1], int(sl[2])
+
+            if len(sl) > 3:
+                print("Ошибка ввода")
+            elif a > 10 or b > 10:
+                print("Вводите числа до 10 включительно")
+            elif b == 0:
+                print("Делить на ноль нельзя")
+            elif op == '+':
+                print(a + b)
+            elif op == '-':
+                print(a - b)
+            elif op == '/':
+                print(a // b)
+            elif op == '*':
+                print(a * b)
+    except Exception:
+        print("Строка не является математической операцией")
+
+
+
+
+main()
